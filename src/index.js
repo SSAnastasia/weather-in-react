09b700e3d5730b8ed0bdb6ea32197250a3262dfe
SSAnastasia <106982689+SSAnastasia@users.noreply.github.com>
2022-09-29 19:22:35 +0300

    added days

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
 import Glory from "./Glory";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
 import Weather from "./Weather";
+import Date from "./Date";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -15,6 +17,7 @@ root.render(
       <div className="container">
         <Glory />
         <Weather />
+        <Date />
         <Forecast />
         <Footer />
       </div>
